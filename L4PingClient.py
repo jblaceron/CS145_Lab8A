6a374454b2e17b334081eba0b3bdf51e07b4c359
Juan Aceron <35886263+jblaceron@users.noreply.github.com>
2019-03-18 22:23:17 +0800

    Add comments.

--- L4PingClient.py
+++ L4PingClient.py
@@ -1,28 +1,42 @@
 import socket, sys
-from threading import Thread, Event
 import time
 from timeit import default_timer as timer
 
 def main():
     if len(sys.argv) >= 2:
+
+        # Get the value of the ping target from the command line arguments
         ADDR = str(sys.argv[1])
         PORT = 6789
         target = (ADDR,PORT)
 
+        # Prepare the client socket
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        clientSocket.settimeout(1.0)
+
+        # The socket will timeout after 2 seconds
+        clientSocket.settimeout(2.0)
+
         for i in range(1,4):
+            # Send the data (1, 2, 3) to the target server
             sent = clientSocket.sendto(str(i),target)
+
+            # Get the current time
             start = timer()
             try:
+
+                # Listen for the value that was sent (1, 2, 3)
                 data = ""
                 while not data:
                     data, server = clientSocket.recvfrom(1024)
+
+                    # If the resent value is correct, print the elapsed time
                     if data == str(i):
                         end = timer()
-                        print 'Packet %s: %f'%(i,end-start)
+                        print 'Packet %s: %.6fms'%(i,(end-start)*1000)
+                    # Else, keep listening for the correct value
                     else:
                         data = ""
+            # When the socket times out, assume the packet is lost
             except:
                 print 'Packet %s: LOST'%i
     else:
--- L4PingServer.py
+++ L4PingServer.py
@@ -1,16 +1,18 @@
-# The server-side code, named L4PingServer.py receives UDP segments
-# from clients, copies the payload, and then sends back a UDP segment
-# to the client with the same payload.
-
 import socket, sys
 
 def main():
+    # Prepare the server socket
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind(('',6789))
 
+
     while True:
+        # Wait for a packet to arrive
         data, address = serverSocket.recvfrom(1024)
         print "Received %s bytes from %s:%s"%(len(data),address[0],address[1])
+        print "    Data: %s"%data
+
+        # Once a packet is received, resend the payload to the sender
         if data:
                 serverSocket.sendto(data,address)
     return
